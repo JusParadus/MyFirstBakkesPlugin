d8edabb309613131dd55683c414a049bb302c8ce
Julian Maierhofer <julian.maierhofer.14@gmail.com>
2025-02-19 20:43:49 +0100

    MyBallOnTop command

--- MyFirstBakkesPlugin/MyFirstBakkesPlugin.cpp
+++ MyFirstBakkesPlugin/MyFirstBakkesPlugin.cpp
@@ -6,44 +6,39 @@ BAKKESMOD_PLUGIN(MyFirstBakkesPlugin, "write a plugin description here", plugin_
 
 std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
 
-void MyFirstBakkesPlugin::onLoad()
-{
+void MyFirstBakkesPlugin::onLoad() {
+	// This line is required for LOG to work and must be before any use of LOG()
 	_globalCvarManager = cvarManager;
-	//LOG("Plugin loaded!");
-	// !! Enable debug logging by setting DEBUG_LOG = true in logging.h !!
-	//DEBUGLOG("MyFirstBakkesPlugin debug mode enabled");
+	LOG("Hello I'm the first real Plugin B) Im loaded");
 
-	// LOG and DEBUGLOG use fmt format strings https://fmt.dev/latest/index.html
-	//DEBUGLOG("1 = {}, 2 = {}, pi = {}, false != {}", "one", 2, 3.14, true);
+	cvarManager->registerNotifier("MyBallOnTop", [this](std::vector<std::string> args) {
+		ballOnTop();
+		}, "", PERMISSION_ALL);
 
 	//cvarManager->registerNotifier("my_aweseome_notifier", [&](std::vector<std::string> args) {
 	//	LOG("Hello notifier!");
 	//}, "", 0);
 
-	//auto cvar = cvarManager->registerCvar("template_cvar", "hello-cvar", "just a example of a cvar");
-	//auto cvar2 = cvarManager->registerCvar("template_cvar2", "0", "just a example of a cvar with more settings", true, true, -10, true, 10 );
+void MyFirstBakkesPlugin::onUnload() {
+	LOG("I was too cool for this world B'(");
+}
 
-	//cvar.addOnValueChanged([this](std::string cvarName, CVarWrapper newCvar) {
-	//	LOG("the cvar with name: {} changed", cvarName);
-	//	LOG("the new value is: {}", newCvar.getStringValue());
-	//});
+void MyFirstBakkesPlugin::ballOnTop() {
+	// Get the game wrapper and check if we are in freeplay
+	if (!gameWrapper->IsInFreeplay()) { return; }
+	ServerWrapper server = gameWrapper->GetGameEventAsServer();
+	if (!server) { return; }
 
-	//cvar2.addOnValueChanged(std::bind(&MyFirstBakkesPlugin::YourPluginMethod, this, _1, _2));
+	// Get the ball and the car and check if they exist
+	BallWrapper ball = server.GetBall();
+	if (!ball) { return; }
+	CarWrapper car = gameWrapper->GetLocalCar();
+	if (!car) { return; }
 
-	// enabled decleared in the header
-	//enabled = std::make_shared<bool>(false);
-	//cvarManager->registerCvar("TEMPLATE_Enabled", "0", "Enable the TEMPLATE plugin", true, true, 0, true, 1).bindTo(enabled);
-
-	//cvarManager->registerNotifier("NOTIFIER", [this](std::vector<std::string> params){FUNCTION();}, "DESCRIPTION", PERMISSION_ALL);
-	//cvarManager->registerCvar("CVAR", "DEFAULTVALUE", "DESCRIPTION", true, true, MINVAL, true, MAXVAL);//.bindTo(CVARVARIABLE);
-	//gameWrapper->HookEvent("FUNCTIONNAME", std::bind(&TEMPLATE::FUNCTION, this));
-	//gameWrapper->HookEventWithCallerPost<ActorWrapper>("FUNCTIONNAME", std::bind(&MyFirstBakkesPlugin::FUNCTION, this, _1, _2, _3));
-	//gameWrapper->RegisterDrawable(bind(&TEMPLATE::Render, this, std::placeholders::_1));
-
-
-	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", [this](std::string eventName) {
-	//	LOG("Your hook got called and the ball went POOF");
-	//});
-	// You could also use std::bind here
-	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", std::bind(&MyFirstBakkesPlugin::YourPluginMethod, this);
+	// Set the ball's velocity to the car's velocity and set the ball's location to the car's location plus the ball's radius
+	Vector carVelocity = car.GetVelocity();
+	ball.SetVelocity(carVelocity);
+	Vector carLocation = car.GetLocation();
+	float ballRadius = ball.GetRadius();
+	ball.SetLocation(carLocation + Vector(0, 0, ballRadius * 1.5));
 }
