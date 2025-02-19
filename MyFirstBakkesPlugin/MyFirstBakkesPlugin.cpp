10dbe75f8405ac230bfb4cddc482d64b947976ff
Julian Maierhofer <julian.maierhofer.14@gmail.com>
2025-02-19 21:09:59 +0100

    Added Enable and minor fixes

--- MyFirstBakkesPlugin/MyFirstBakkesPlugin.cpp
+++ MyFirstBakkesPlugin/MyFirstBakkesPlugin.cpp
@@ -2,9 +2,10 @@
 #include "MyFirstBakkesPlugin.h"
 
 
-BAKKESMOD_PLUGIN(MyFirstBakkesPlugin, "write a plugin description here", plugin_version, PLUGINTYPE_FREEPLAY)
+BAKKESMOD_PLUGIN(MyFirstBakkesPlugin, "This is a plugin description!", plugin_version, PLUGINTYPE_FREEPLAY)
 
 std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
+bool coolEnabled = false;
 
 void MyFirstBakkesPlugin::onLoad() {
 	// This line is required for LOG to work and must be before any use of LOG()
@@ -15,9 +16,13 @@ void MyFirstBakkesPlugin::onLoad() {
 		ballOnTop();
 		}, "", PERMISSION_ALL);
 
-	//cvarManager->registerNotifier("my_aweseome_notifier", [&](std::vector<std::string> args) {
-	//	LOG("Hello notifier!");
-	//}, "", 0);
+	cvarManager->registerCvar("cool_enabled", "0", "Enable Cool", true, true, 0, true, 1)
+		.addOnValueChanged([this](std::string oldValue, CVarWrapper cvar) {
+		coolEnabled = cvar.getBoolValue();
+			});
+
+	cvarManager->registerCvar("cool_distance", "200.0", "Distance to place the ball above");
+}
 
 void MyFirstBakkesPlugin::onUnload() {
 	LOG("I was too cool for this world B'(");
@@ -28,6 +33,12 @@ void MyFirstBakkesPlugin::ballOnTop() {
 	if (!gameWrapper->IsInFreeplay()) { return; }
 	ServerWrapper server = gameWrapper->GetGameEventAsServer();
 	if (!server) { return; }
+
+	if(!coolEnabled) { return; }
+
+	CVarWrapper distanceCVar = cvarManager->getCvar("cool_distance");
+	if (!distanceCVar) { return; }
+	float distance = distanceCVar.getFloatValue();
 
 	// Get the ball and the car and check if they exist
 	BallWrapper ball = server.GetBall();
@@ -40,5 +51,5 @@ void MyFirstBakkesPlugin::ballOnTop() {
 	ball.SetVelocity(carVelocity);
 	Vector carLocation = car.GetLocation();
 	float ballRadius = ball.GetRadius();
-	ball.SetLocation(carLocation + Vector(0, 0, ballRadius * 1.5));
+	ball.SetLocation(carLocation + Vector(0, 0, ballRadius * 1.6));
 }
