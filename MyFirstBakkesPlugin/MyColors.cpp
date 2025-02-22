01056db3371da092ceaeeee0bee7626c2140e140
Julian Maierhofer <maierhofer.julian10@gmail.com>
2025-02-22 14:18:39 +0100

    Added MyColors class

---
+++ MyFirstBakkesPlugin/MyColors.cpp
@@ -0,0 +1,56 @@
+#include "pch.h"
+#include "MyColors.h"
+
+// Basic Colors
+const LinearColor MyColors::RED = { 255, 0, 0, 255 };
+const LinearColor MyColors::GREEN = { 0, 255, 0, 255 };
+const LinearColor MyColors::BLUE = { 0, 0, 255, 255 };
+const LinearColor MyColors::WHITE = { 255, 255, 255, 255 };
+const LinearColor MyColors::BLACK = { 0, 0, 0, 255 };
+
+// Gray Shades
+const LinearColor MyColors::GRAY = { 128, 128, 128, 255 };
+const LinearColor MyColors::LIGHT_GRAY = { 200, 200, 200, 255 };
+const LinearColor MyColors::DARK_GRAY = { 64, 64, 64, 255 };
+
+// Extended Colors
+const LinearColor MyColors::CYAN = { 0, 255, 255, 255 };
+const LinearColor MyColors::MAGENTA = { 255, 0, 255, 255 };
+const LinearColor MyColors::YELLOW = { 255, 255, 0, 255 };
+const LinearColor MyColors::ORANGE = { 255, 165, 0, 255 };
+const LinearColor MyColors::PURPLE = { 128, 0, 128, 255 };
+const LinearColor MyColors::PINK = { 255, 192, 203, 255 };
+const LinearColor MyColors::BROWN = { 139, 69, 19, 255 };
+const LinearColor MyColors::LIME = { 50, 205, 50, 255 };
+const LinearColor MyColors::GOLD = { 255, 215, 0, 255 };
+const LinearColor MyColors::SILVER = { 192, 192, 192, 255 };
+
+// More Color Variations
+const LinearColor MyColors::MAROON = { 128, 0, 0, 255 };
+const LinearColor MyColors::NAVY = { 0, 0, 128, 255 };
+const LinearColor MyColors::TEAL = { 0, 128, 128, 255 };
+const LinearColor MyColors::OLIVE = { 128, 128, 0, 255 };
+const LinearColor MyColors::INDIGO = { 75, 0, 130, 255 };
+const LinearColor MyColors::VIOLET = { 238, 130, 238, 255 };
+const LinearColor MyColors::TURQUOISE = { 64, 224, 208, 255 };
+const LinearColor MyColors::CORAL = { 255, 127, 80, 255 };
+const LinearColor MyColors::SALMON = { 250, 128, 114, 255 };
+const LinearColor MyColors::CHOCOLATE = { 210, 105, 30, 255 };
+const LinearColor MyColors::TAN = { 210, 180, 140, 255 };
+const LinearColor MyColors::CRIMSON = { 220, 20, 60, 255 };
+const LinearColor MyColors::PLUM = { 221, 160, 221, 255 };
+const LinearColor MyColors::BEIGE = { 245, 245, 220, 255 };
+const LinearColor MyColors::LAVENDER = { 230, 230, 250, 255 };
+const LinearColor MyColors::MINT = { 189, 252, 201, 255 };
+const LinearColor MyColors::SKY_BLUE = { 135, 206, 235, 255 };
+const LinearColor MyColors::FOREST_GREEN = { 34, 139, 34, 255 };
+const LinearColor MyColors::SEA_GREEN = { 46, 139, 87, 255 };
+const LinearColor MyColors::MIDNIGHT_BLUE = { 25, 25, 112, 255 };
+
+// Transparency Variations
+const LinearColor MyColors::TRANSPARENT = { 0, 0, 0, 0 };
+const LinearColor MyColors::SEMI_TRANSPARENT = { 0, 0, 0, 128 };
+const LinearColor MyColors::SEMI_RED = { 255, 0, 0, 128 };
+const LinearColor MyColors::SEMI_BLUE = { 0, 0, 255, 128 };
+const LinearColor MyColors::SEMI_GREEN = { 0, 255, 0, 128 };
+const LinearColor MyColors::SEMI_WHITE = { 255, 255, 255, 128 };
---
+++ MyFirstBakkesPlugin/MyColors.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#ifndef MYCOLORS_H
+#define MYCOLORS_H
+
+class MyColors {
+public:
+    // Basic Colors
+    static const LinearColor RED;
+    static const LinearColor GREEN;
+    static const LinearColor BLUE;
+    static const LinearColor WHITE;
+    static const LinearColor BLACK;
+
+    // Gray Shades
+    static const LinearColor GRAY;
+    static const LinearColor LIGHT_GRAY;
+    static const LinearColor DARK_GRAY;
+
+    // Extended Colors
+    static const LinearColor CYAN;
+    static const LinearColor MAGENTA;
+    static const LinearColor YELLOW;
+    static const LinearColor ORANGE;
+    static const LinearColor PURPLE;
+    static const LinearColor PINK;
+    static const LinearColor BROWN;
+    static const LinearColor LIME;
+    static const LinearColor GOLD;
+    static const LinearColor SILVER;
+
+    // More Color Variations
+    static const LinearColor MAROON;
+    static const LinearColor NAVY;
+    static const LinearColor TEAL;
+    static const LinearColor OLIVE;
+    static const LinearColor INDIGO;
+    static const LinearColor VIOLET;
+    static const LinearColor TURQUOISE;
+    static const LinearColor CORAL;
+    static const LinearColor SALMON;
+    static const LinearColor CHOCOLATE;
+    static const LinearColor TAN;
+    static const LinearColor CRIMSON;
+    static const LinearColor PLUM;
+    static const LinearColor BEIGE;
+    static const LinearColor LAVENDER;
+    static const LinearColor MINT;
+    static const LinearColor SKY_BLUE;
+    static const LinearColor FOREST_GREEN;
+    static const LinearColor SEA_GREEN;
+    static const LinearColor MIDNIGHT_BLUE;
+
+    // Transparency Variations
+    static const LinearColor TRANSPARENT;
+    static const LinearColor SEMI_TRANSPARENT;
+    static const LinearColor SEMI_RED;
+    static const LinearColor SEMI_BLUE;
+    static const LinearColor SEMI_GREEN;
+    static const LinearColor SEMI_WHITE;
+};
+
+#endif // MYCOLORS_H
